26c8daa0cd563899dbfef8c35759ebbf92ab2edd
Brian Jewkes <bdjewkes@gmail.com>
2019-07-25 21:02:15 -0700

    Remove cruft

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import time
-from threading import Thread
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 import server.ticker as ticker
@@ -7,7 +5,7 @@ import server.ticker as ticker
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'notarealsecret'
-socketio = SocketIO(app, async_mode='gevent')
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -44,7 +42,7 @@ DEBUG_HOST = '127.0.0.1'
 DEBUG_PORT = 5000
 if __name__ == '__main__':
     print(f"Starting server at {DEBUG_HOST}:{DEBUG_PORT}")
-    socketio.start_background_task(ticker.run_ticker, socketio)
+    socketio.start_background_task(ticker.run_ticker)
     socketio.run(app, use_reloader=True, debug=True, log_output=True,
                  host=DEBUG_HOST, port=DEBUG_PORT)
 
