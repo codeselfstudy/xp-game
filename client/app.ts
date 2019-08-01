876d7ee4611e68925805f93947d8117f9f55b4cb
J. Cohen <hostelmarketing@gmail.com>
2019-07-31 20:57:18 -0700

    Fix more formatting

--- client/app.ts
+++ client/app.ts
@@ -55,7 +55,7 @@ export function initialize() {
 }
 
 function update(c: Canvas, world: World, scale: number, clientId?: string) {
-    c.ctx.clearRect(0, 0, c.canvas.width, c.canvas.height);
+    c.ctx.clearRect(0,0, c.canvas.width, c.canvas.height);
     drawGrid(c, scale);
     world.entities.forEach((e) => {
         draw(c, e, scale, clientId);
@@ -90,7 +90,7 @@ function drawGrid(c: Canvas, scale: number){
 function draw(c: Canvas, thing: Entity, scale: number, clientId: string){
     c.ctx.beginPath();
     c.ctx.rect(thing.position.x*scale, thing.position.y*scale,
-        scale, scale);
+               scale, scale);
     c.ctx.fillStyle = clientId == thing.client_id ? "blue" : "red";
     c.ctx.fill();
     c.ctx.closePath();
